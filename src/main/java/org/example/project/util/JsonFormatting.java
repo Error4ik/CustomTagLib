0d9ceb4b49d258bb09e1c5b9832b1c9c7e461dda
Aleksey Voronin <Aleksei_Voronin1@epam.com>
2021-10-27 23:20:09 +0300

    Json

--- src/main/java/org/example/project/util/JsonFormatting.java
+++ src/main/java/org/example/project/util/JsonFormatting.java
@@ -1,141 +1,142 @@
 package org.example.project.util;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.example.project.service.JsonService;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 public class JsonFormatting {
 
-    private static final Logger logger = LogManager.getLogger();
     private static final String newLine = "\n";
-    private static final String whitespace = "";
-
-    private static final String arrayStart = "<span style='color:blue'>[</span>";
-    private static final String arrayEnd = "<span style='color:blue'>]</span>";
-    private static final String objectStart = "<span style='color:red'>{</span>";
-    private static final String objectEnd = "<span style='color:red'>}</span>";
-
-    private static final String keyStyle = "<span style='color:green'>%s%s%s:</span>";
-    private static final String valueStyle = "<span style='color:orange'>%s%s%s</span>";
-
     private static final String qMarks = "\"";
+    private static final String arrayStart = "<span style='color:black'>[</span>";
+    private static final String arrayEnd = "<span style='color:black'>]</span>";
+    private static final String objectStart = "<span style='color:black'>{</span>";
+    private static final String objectEnd = "<span style='color:black'>}</span>";
+    private static final String keyStyle = "<span style='color:purple'>%s%s%s:</span>";
+    private static final String valueStyle = "<span style='color:green'>%s%s%s</span>";
+    private static final String numberOrBooleanValueStyle = "<span style='color:blue'>%s</span>";
 
-    private static final StringBuilder sb = new StringBuilder();
-    private static int level = 1;
+    private int depthLevel = 0;
 
-    public static String format(String json) {
+    public String format(String json) {
         StringBuilder sb = new StringBuilder();
-        JSONArray jsonArray = new JSONArray(json);
-//        int level = 1;
-        sb.append(arrayStart);
-        sb.append(newLine);
-
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
-            Set<String> strings = jsonObject.keySet();
-            sb.append(getWhitespace(level));
-            sb.append(objectStart);
-            sb.append(newLine);
-            level += 1;
-            int count = 0;
-            for (String string : strings) {
-                Object o = jsonObject.get(string);
-                sb.append(getWhitespace(level));
-                sb.append(String.format(" %s", String.format(keyStyle, qMarks, string, qMarks)));
-
-                if (o instanceof JSONArray) {
-                    sb.append(String.format(" %s", arrayStart));
-                    sb.append(newLine);
-                    JSONArray innerArr = (JSONArray) o;
-
-                    level += 2;
-                    for (int j = 0; j < innerArr.length(); j++) {
-                        JSONObject innerOb = innerArr.getJSONObject(j);
-                        Set<String> innerStrings = innerOb.keySet();
-                        level += 1;
-                        sb.append(getWhitespace(level));
-                        sb.append(objectStart);
-                        sb.append(newLine);
-                        level += 2;
-                        int count2 = 0;
-                        for (String innerString : innerStrings) {
-                            Object ob = innerOb.get(innerString);
-                            sb.append(getWhitespace(level));
-                            String innerKey = String.format(keyStyle, qMarks, innerString, qMarks);
-                            String innerValue;
-                            if (ob instanceof Number) {
-                                innerValue = String.format("<span style='color:blue'>%s</span>", ob);
-                            } else {
-                                innerValue = String.format(valueStyle, qMarks, ob, qMarks);
-                            }
-                            sb.append(String.format("%s %s", innerKey, innerValue));
-                            if (count2 + 1 < innerString.length()) {
-                                sb.append(",");
-                            }
-                            sb.append(newLine);
-                            count2++;
-                        }
-                        level -= 2;
-                        sb.append(getWhitespace(level));
-                        sb.append(objectEnd);
-
-                        if (j + 1 < innerArr.length()) {
-                            sb.append(",");
-                        }
-                        sb.append(newLine);
-                        level -= 1;
-                    }
-                    sb.append(getWhitespace(level - 1));
-                    sb.append(arrayEnd);
-                    if (count + 1 < strings.size()) {
-                        sb.append(",");
-                    }
-                    sb.append(newLine);
-                    level -= 2;
-                } else {
-                    String value;
-                    if (o instanceof Number) {
-                        value = String.format("<span style='color:blue'>%s</span>", o);
-                    } else {
-                        value = String.format(valueStyle, qMarks, o, qMarks);
-                    }
-                    sb.append(String.format(" %s", value));
-                    if (count + 1 < strings.size()) {
-                        sb.append(",");
-                    }
-                    sb.append(newLine);
-                }
-                count++;
-            }
-            level -= 1;
-            sb.append(getWhitespace(level));
-            sb.append(objectEnd);
-
-            if (i + 1 < jsonArray.length()) {
-                sb.append(",");
-                sb.append(newLine);
-            }
+        if (json.startsWith("[")) {
+            JSONArray jsonArray = new JSONArray(json);
+            printArray(sb, jsonArray, false);
+        } else {
+            JSONObject jsonObject = new JSONObject(json);
+            printObject(sb, jsonObject, false, false);
         }
-
-        sb.append(newLine);
-        sb.append(arrayEnd);
-        sb.append(newLine);
-
         return sb.toString();
     }
 
-    private static String getWhitespace(int quantity) {
-        StringBuilder a = new StringBuilder(whitespace);
+    private void printArray(StringBuilder sb, JSONArray jsonArray, boolean hasMoreElements) {
+        sb.append(arrayStart);
+        sb.append(newLine);
+        increaseDepthLevel(2);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            Object o = jsonArray.get(i);
+            if (o instanceof JSONObject) {
+                JSONObject jsonObject = (JSONObject) o;
+                printObject(sb, jsonObject, i + 1 < jsonArray.length(), false);
+            } else if (o instanceof JSONArray) {
+                JSONArray arr = (JSONArray) o;
+                printArray(sb, arr, i + 1 < jsonArray.length());
+            } else {
+                printList(sb, o, i + 1 < jsonArray.length());
+            }
+        }
+        decreaseDepthLevel(2);
+        sb.append(getWhitespace(depthLevel));
+        sb.append(arrayEnd);
+        if (hasMoreElements) {
+            sb.append(",");
+        }
+        sb.append(newLine);
+    }
+
+    private void printObject(StringBuilder sb, JSONObject jsonObject, boolean hasMoreElement, boolean isRecursive) {
+        Set<String> strings = jsonObject.keySet();
+        if (!isRecursive) {
+            sb.append(getWhitespace(depthLevel));
+        }
+        sb.append(objectStart);
+        sb.append(newLine);
+        increaseDepthLevel(1);
+        int count = 0;
+
+        for (String string : strings) {
+            Object o = jsonObject.get(string);
+            sb.append(getWhitespace(depthLevel));
+            sb.append(String.format(" %s", String.format(keyStyle, qMarks, string, qMarks)));
+            increaseDepthLevel(2);
+            if (o instanceof JSONArray) {
+                decreaseDepthLevel(1);
+                JSONArray arr = (JSONArray) o;
+                printArray(sb, arr, count + 1 < strings.size());
+                increaseDepthLevel(1);
+            } else if (o instanceof JSONObject) {
+                decreaseDepthLevel(1);
+                JSONObject jObj = (JSONObject) o;
+                printObject(sb, jObj, count + 1 < strings.size(), true);
+                increaseDepthLevel(1);
+            } else {
+                if (checkIsNumberOrBoolean(o)) {
+                    sb.append(String.format(numberOrBooleanValueStyle, o));
+                } else {
+                    sb.append(String.format(valueStyle, qMarks, o, qMarks));
+                }
+                if (count + 1 < strings.size()) {
+                    sb.append(",");
+                }
+                sb.append(newLine);
+            }
+            decreaseDepthLevel(2);
+            count++;
+        }
+
+        decreaseDepthLevel(1);
+        sb.append(getWhitespace(depthLevel));
+        sb.append(objectEnd);
+
+        if (hasMoreElement) {
+            sb.append(",");
+        }
+        sb.append(newLine);
+    }
+
+    private void printList(StringBuilder sb, Object o, boolean hasMoreElements) {
+        sb.append(getWhitespace(depthLevel));
+        if (checkIsNumberOrBoolean(o)) {
+            sb.append(String.format(numberOrBooleanValueStyle, o));
+        } else {
+            sb.append(String.format(valueStyle, qMarks, o, qMarks));
+        }
+
+        if (hasMoreElements) {
+            sb.append(",");
+        }
+        sb.append(newLine);
+    }
+
+    private boolean checkIsNumberOrBoolean(Object o) {
+        return o instanceof Number || o instanceof Boolean;
+    }
+
+    private String getWhitespace(int quantity) {
+        StringBuilder a = new StringBuilder("");
         for (int i = 0; i < quantity; i++) {
             a.append(" ");
         }
         return a.toString();
+    }
+
+    private void increaseDepthLevel(int quantity) {
+        depthLevel += quantity;
+    }
+
+    private void decreaseDepthLevel(int quantity) {
+        depthLevel -= quantity;
     }
 }
